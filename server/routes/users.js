1a299810d84f5a69ee34610bc25f7be80238205c
Ryan Lazzareschi <57603530+ryanlazz16@users.noreply.github.com>
2020-11-21 10:13:13 -0800

    Started routes and databse NOT DEBUGGED

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,369 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql')
+const cookieParser = require('cookie-parser');
+const app = require('../app');
+const uuid = require('uuid');
+require('dotenv').config();
+const cfg = require('../../ModelBuilder/config/config.json');
+const Stocks = require('../../ModelBuilder/DataScraper/DataScraper.js');
+
+router.use(cookieParser())
+
+const db = mysql.createConnection({
+  host: 'localhost', 
+  user: process.env.dbUser, 
+  password: process.env.dbPassword,
+  database: 'nodemysql'
+})
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySql Connected...');
+});
+
+// // unecessary if database is specified in createConnection funciton
+// router.get('/createdb', (req, res) => {
+//   let sql = 'CREATE DATABASE nodemysql';
+//   db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send('Database created');
+//   });
+// });
+
+const cookie = 'cookie';
+
+router.get('/createTable', (req, res) => {
+  let sql = 'CREATE TABLE ModelData(id VARCHAR(255), model VARCHAR(MAX), PRIMARY KEY(id))';
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('table created...')
+  });
+});
+
+// for debugging
+router.get('/getrow/:id', (req, res) => {
+  let sql = `SELECT * FROM ModelData WHERE id = ${req.params.id}`;
+  let query = db.query(sql, (err, result, fields) => {
+    if (err) throw err;
+    console.log(result);
+    res.json(fields);
+  });
+});
+
+function setLimit(req, res) {
+  let sql = 'SELECT model FROM ModelData WHERE id = ?';
+
+  // need to handle if there is no row with id
+  db.query(sql, req.cookies[cookie], (err, result, fields) => {
+    if (err) throw err;
+
+    let model = fields[0].model;
+    model = JSON.parse(model);
+    let model = new Stocks.DataScraper(model.stocks, model.params, modle.period, model.limit);
+
+    model.setLimit(req.body.limit);
+    model = JSON.stringify(model)
+
+    let sql = 'UPDATE ModelData SET moodel = ? WHERE id = ?';
+    db.query(sql, model, req.cookies[cookie], (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Set Limit')
+    });
+  })
+}
+
+router.post('/setLimit', (req, res) => {
+  if (req.cookies[cookie]==undefined) {
+    var id = uuid.v4();
+    res.cookie(cookie, id, { expires: new Date(Date.now() + 3600000), httpOnly: true });
+
+    let model = new Stocks.DataScraper();
+    model = JSON.stringify(model);
+
+    let sql = 'INSERT INTO ModelData Values (?, ?)';
+
+    db.query(sql, id, model, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+
+      setLimit(req, res);
+    });
+  } else {
+    setLimit(req, res);
+  }
+});
+
+function setPeriod(req, res) {
+  let sql = 'SELECT model FROM ModelData WHERE id = ?';
+
+  // need to handle if there is no row with id
+  db.query(sql, req.cookies[cookie], (err, result, fields) => {
+    if (err) throw err;
+
+    let model = fields[0].model;
+    model = JSON.parse(model);
+    let model = new Stocks.DataScraper(model.stocks, model.params, modle.period, model.limit);
+
+    model.setPeriod(req.body.period);
+    model = JSON.stringify(model)
+
+    let sql = 'UPDATE ModelData SET moodel = ? WHERE id = ?';
+    db.query(sql, model, req.cookies[cookie], (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Set Period')
+    });
+  })
+}
+
+router.post('/setPeriod', (req, res) => {
+  if (req.cookies[cookie]==undefined) {
+    var id = uuid.v4();
+    res.cookie(cookie, id, { expires: new Date(Date.now() + 3600000), httpOnly: true });
+
+    let model = new Stocks.DataScraper();
+    model = JSON.stringify(model);
+
+    let sql = 'INSERT INTO ModelData Values (?, ?)';
+
+    db.query(sql, id, model, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+
+      setPeriod(req, res);
+    });
+  } else {
+    setPeriod(req, res);
+  }
+});
+
+function addParams(req, res) {
+  let sql = 'SELECT model FROM ModelData WHERE id = ?';
+
+  // need to handle if there is no row with id
+  db.query(sql, req.cookies[cookie], (err, result, fields) => {
+    if (err) throw err;
+
+    let model = fields[0].model;
+    model = JSON.parse(model);
+    let model = new Stocks.DataScraper(model.stocks, model.params, modle.period, model.limit);
+
+    model.addParams(req.body.params);
+    model = JSON.stringify(model)
+
+    let sql = 'UPDATE ModelData SET moodel = ? WHERE id = ?';
+    db.query(sql, model, req.cookies[cookie], (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Added params')
+    });
+  })]
+}
+
+router.post('/addParams', (req, res) => {
+  if (req.cookies[cookie]==undefined) {
+    var id = uuid.v4();
+    res.cookie(cookie, id, { expires: new Date(Date.now() + 3600000), httpOnly: true });
+
+    let model = new Stocks.DataScraper();
+    model = JSON.stringify(model);
+
+    let sql = 'INSERT INTO ModelData Values (?, ?)';
+
+    db.query(sql, id, model, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+
+      addParams(req, res);
+    });
+  } else {
+    addParams(req, res);
+  }
+});
+
+function removeParams(req, res) {
+  let sql = 'SELECT model FROM ModelData WHERE id = ?';
+
+  // need to handle if there is no row with id
+  db.query(sql, req.cookies[cookie], (err, result, fields) => {
+    if (err) throw err;
+
+    let model = fields[0].model;
+    model = JSON.parse(model);
+    let model = new Stocks.DataScraper(model.stocks, model.params, modle.period, model.limit);
+
+    model.removeParams(req.body.params);
+    model = JSON.stringify(model)
+
+    let sql = 'UPDATE ModelData SET moodel = ? WHERE id = ?';
+    db.query(sql, model, req.cookies[cookie], (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Removed params')
+    });
+  })
+  });
+}
+
+router.post('/removeParams', (req, res) => {
+  if (req.cookies[cookie]==undefined) {
+    var id = uuid.v4();
+    res.cookie(cookie, id, { expires: new Date(Date.now() + 3600000), httpOnly: true });
+
+    let model = new Stocks.DataScraper();
+    model = JSON.stringify(model);
+
+    let sql = 'INSERT INTO ModelData Values (?, ?)';
+
+    db.query(sql, id, model, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+
+      removeParams(req, res);
+    });
+  } else {
+    removeParams(req, res);
+  }
+});
+
+function addStocks(req, res) {
+  let sql = 'SELECT model FROM ModelData WHERE id = ?';
+
+  // need to handle if there is no row with id
+  db.query(sql, req.cookies[cookie], (err, result, fields) => {
+    if (err) throw err;
+
+    let model = fields[0].model;
+    model = JSON.parse(model);
+    let model = new Stocks.DataScraper(model.stocks, model.params, modle.period, model.limit);
+
+    model.addStocks(req.body.stocks);
+    model = JSON.stringify(model)
+
+    let sql = 'UPDATE ModelData SET moodel = ? WHERE id = ?';
+    db.query(sql, model, req.cookies[cookie], (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Added stocks')
+    });
+  })
+}
+
+router.post('/addStocks', (req, res) => {
+  if (req.cookies[cookie]==undefined) {
+    var id = uuid.v4();
+    res.cookie(cookie, id, { expires: new Date(Date.now() + 3600000), httpOnly: true });
+
+    let model = new Stocks.DataScraper();
+    model = JSON.stringify(model);
+
+    let sql = 'INSERT INTO ModelData Values (?, ?)';
+
+    db.query(sql, id, model, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+
+      addStocks(req, res);
+    });
+  } else {
+    addStocks(req, res);
+  }
+});
+
+function removeStocks(req, res) {
+  let sql = 'SELECT model FROM ModelData WHERE id = ?';
+
+  // need to handle if there is no row with id
+  db.query(sql, req.cookies[cookie], (err, result, fields) => {
+    if (err) throw err;
+
+    let model = fields[0].model;
+    model = JSON.parse(model);
+    let model = new Stocks.DataScraper(model.stocks, model.params, modle.period, model.limit);
+
+    model.removeStocks(req.body.stocks);
+    model = JSON.stringify(model)
+
+    let sql = 'UPDATE ModelData SET moodel = ? WHERE id = ?';
+    db.query(sql, model, req.cookies[cookie], (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Removed stocks')
+    });
+  })
+}
+
+router.post('/removeStocks', (req, res) => {
+  if (req.cookies[cookie]==undefined) {
+    var id = uuid.v4();
+    res.cookie(cookie, id, { expires: new Date(Date.now() + 3600000), httpOnly: true });
+
+    let model = new Stocks.DataScraper();
+    model = JSON.stringify(model);
+
+    let sql = 'INSERT INTO ModelData Values (?, ?)';
+
+    db.query(sql, id, model, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+
+      removeStocks(req, res);
+    });
+  } else {
+    removeStocks(req, res);
+  }
+});
+
+function generateModel(req, res) {
+  let sql = 'SELECT model FROM ModelData WHERE id = ?';
+
+  // need to handle if there is no row with id
+  db.query(sql, req.cookies[cookie], (err, result, fields) => {
+    if (err) throw err;
+
+    let model = fields[0].model;
+    model = JSON.parse(model);
+    let model = new Stocks.DataScraper(model.stocks, model.params, modle.period, model.limit);
+
+    model.generateModel().then((m) => {
+      model = JSON.stringify(model)
+
+      let sql = 'UPDATE ModelData SET moodel = ? WHERE id = ?';
+      db.query(sql, model, req.cookies[cookie], (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.json(m)
+      });
+   });
+  })
+}
+
+router.get('/generateModel', (req, res) => {
+  if (req.cookies[cookie]==undefined) {
+    var id = uuid.v4();
+    res.cookie(cookie, id, { expires: new Date(Date.now() + 3600000), httpOnly: true });
+
+    let model = new Stocks.DataScraper();
+    model = JSON.stringify(model);
+
+    let sql = 'INSERT INTO ModelData Values (?, ?)';
+
+    db.query(sql, id, model, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+
+      generateModel(req, res);
+    });
+  } else {
+    generateModel(req, res);
+  }
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
