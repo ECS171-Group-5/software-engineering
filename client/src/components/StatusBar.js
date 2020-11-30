6232567ca5df258c2436a1fc87f0c814649ccc22
Nathaniel Kong <nskong@ucdavis.edu>
2020-11-29 19:25:34 -0800

    Pick a stock for select table

--- client/src/components/StatusBar.js
+++ client/src/components/StatusBar.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 
-
-// TODO buttons not hooked up
 export default class StatusBar extends Component {
     render() {
         return (
             <div id='status-bar'>
                 <div id='select'>SELECT</div>
-                <div id='configure'>CONFIGURE</div>
-                <div id='RESULT'>RESULT</div>
+                <div id='result'>RESULT</div>
             </div>
         );
     } 
--- client/src/components/StockTable.js
+++ client/src/components/StockTable.js
@@ -5,8 +5,6 @@ import { TableHead } from '@material-ui/core';
 import { TableCell } from '@material-ui/core';
 import { TableBody } from '@material-ui/core';
 
-// const titleRow = ['COMPANY', 'NASDAQ', 'START DATE', 'END DATE'];
-
 const sampleData = [
     {company:'Zoom', nasdaq:'ZM', startDate:'2019-04-30', endDate:'2020-07-31'},
     {company:'Zoom2', nasdaq:'ZM', startDate:'2019-04-30', endDate:'2020-07-31'},
@@ -22,33 +20,79 @@ const sampleData = [
 ]
 
 export default class StockTable extends Component {
+    constructor() { 
+        super();
+        this.state = {
+            selected:new Array(sampleData.length).fill(false)
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.isSelected = this.isSelected.bind(this);
+    }
+
+    /* commented out because was reaching max update limit, if optimized can use */
+    handleHover = (index, highlight) => { 
+    //     let newSelected = new Array(sampleData.length).fill(false);
+    //     if (highlight) { 
+    //         newSelected[index] = true;
+    //     }
+    //     this.selected = newSelected;
+    //     this.setState({
+    //         selected:newSelected
+    //     })
+    }
+
+    handleClick = (event, index) => {
+        let newSelected = new Array(sampleData.length).fill(false);
+        newSelected[index] = true;
+        this.selected = newSelected;
+        this.setState({
+            selected:newSelected
+        })
+    };
+
+    /* Function returns if a row of index i is selected */
+    isSelected = (i) => {
+        return this.state.selected[i];
+    };
+
     render() {
         return (
             <div className='tableContainer'>
 
                 <div className='table-title medium'>Step 1: Select a stock from our dataset</div>
 
-                <Table className='table-rows'>
+                <Table 
+                    className='table-rows'
+                    stickyHeader
+                >
                     <TableHead>
-                        <TableRow>
+                        <TableRow className='table-head'>
                             <TableCell>COMPANY</TableCell>
-                            <TableCell align="right">NASDAQ</TableCell>
-                            <TableCell align="right">START DATE</TableCell>
-                            <TableCell align="right">END DATE</TableCell>
+                            <TableCell align='right'>NASDAQ</TableCell>
+                            <TableCell align='right'>START DATE</TableCell>
+                            <TableCell align='right'>END DATE</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {sampleData.map((row, i) => (
-                            // change the key from i to row.company when done
-                            <TableRow key={i}>
-                                <TableCell component="th" scope="row">
-                                    {row.company}
-                                </TableCell>
-                                <TableCell align="right">{row.nasdaq}</TableCell>
-                                <TableCell align="right">{row.startDate}</TableCell>
-                                <TableCell align="right">{row.endDate}</TableCell>
-                            </TableRow>
-                        ))}
+                        {sampleData.map((row, i) => {
+                            const isItemSelected = this.isSelected(i);
+
+                            return (
+                                <TableRow 
+                                    key={i} 
+                                    onMouseEnter={this.handleHover(i, true)} 
+                                    onMouseLeave={this.handleHover(i, false)}
+                                    onClick={(event) => this.handleClick(event, i)}
+                                    className={isItemSelected ? 'table-row-selected' : 'table-row-unselected'}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {row.company}
+                                    </TableCell>
+                                    <TableCell align="right">{row.nasdaq}</TableCell>
+                                    <TableCell align="right">{row.startDate}</TableCell>
+                                    <TableCell align="right">{row.endDate}</TableCell>
+                                </TableRow>
+                        )})}
                     </TableBody>
                 </Table>
                 <div id='confirm-button-wrapper'>
